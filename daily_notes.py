1f5c6b67931cc5a584bc670065ad4daad4b98b0a
Surya Ahuja <sahuja4@fb.com>
2021-05-16 19:57:05 -0700

    Added function for removing todo duplicates

--- daily_notes.py
+++ daily_notes.py
@@ -1,5 +1,10 @@
 #!/usr/local/bin/python3.9
-
+"""
+Philosophy
+Keep what should be in focus at the top
+If start date is in the future, then don't show it on the current daily note
+I will think about it when the time comes
+"""
 import datetime
 import re
 import os
@@ -32,7 +37,9 @@ DATE_PATTERNS = {
 OPEN_TASK_PATTERN=r"(\s*)-\s+\[(\s|\>)\]\s*(!*)\s*(.*)"
 CLOSED_TASK_PATTERN=r"\[x\](.*)"
 SHOULD_ARCHIVE=True
+HIDE_FUTURE_TODOS_FROM_DAILY_NOTE=True
 SHAME_THRESHOLD=5
+STICKY_CHAR = "~S~"
 
 class DateNotSupported(Exception):
     pass
@@ -44,8 +51,16 @@ class DateTextNotFound(Exception):
     pass
 
 class Action(Enum):
-    SHAME = 1
-    ARCHIVE = 2
+    # start a fresh todo
+    START = 1
+    # set up todo for shaming
+    SHAME = 2
+    # archive the todo
+    ARCHIVE = 3
+    # protect the todo from being shamed/archived, ever
+    STICK = 4
+    # resurface todo in the future
+    FUTURE = 5
 
 class State(Enum):
     OPEN = 1
@@ -64,8 +79,9 @@ notename should be the title of the note
 """
 
 class Todo:
-    def __init__(self, raw_text, front_spaces="", todo_marker="x", todo_shame="", todo_text=""):
+    def __init__(self, raw_text, notename, front_spaces="", todo_marker="x", todo_shame="", todo_text=""):
         self.raw_text = raw_text
+        self.note_for_todo = notename
         self.front_spaces = front_spaces
         self.marker = f"[{todo_marker}]"
         self.shame = todo_shame
@@ -74,18 +90,65 @@ class Todo:
         self.state = todo_state_map.get(self.marker, State.OPEN)
         self.upcoming_shame=""
         self.action=None
+        # name of the daily note on which this task will be started
+        self.start_date=None
+        self.set_start_date()
         self.plan_next_action()
 
+    def set_start_date(self):
+        extracted_date = self.get_start_date_from_todo_text()
+        if not extracted_date:
+            self.start_date = get_date_from_note_name(get_note_name_for("tomorrow")).date()
+        else:
+            self.start_date = get_date_from_note_name(extracted_date).date()
+
+    def get_start_date_from_todo_text(self):
+        """The first [[<daily_note>]] which follows the text will be the start_date note name"""
+        start_date_regex = r"\[\[(.*)\]\]"
+        matches = re.findall(start_date_regex, self.raw_text)
+        return matches[0] if matches else None
+
+    def is_start_date_tomorrow(self) -> bool:
+        today = datetime.date.today()
+        tomorrow = add_day_delta(today, 1)
+        return self.start_date == tomorrow
+
+    def is_start_date_in_future_but_not_tomorrow(self) -> bool:
+        """If start date of the todo is in the future, return True
+        """
+        today = datetime.date.today()
+        tomorrow = add_day_delta(today, 1)
+        return today < self.start_date and tomorrow != self.start_date
+
     def plan_next_action(self):
+        # if user wants the note to stick then don't add shame to it
+        # sticy notes have preference over future dates because
+        # sticky notes shouldn't have future dates but they could
+        if STICKY_CHAR in self.text:
+            self.action = Action.STICK
+            self.upcoming_shame = ""
+            return
+
+        # if start date is set to a future date but not tomorrow, then don't add shame
+        # and set it up for resurfacing later
+        if self.is_start_date_in_future_but_not_tomorrow():
+            self.action = Action.FUTURE
+            self.upcoming_shame = ""
+            return
+
         # if there is no shame, init to 0 len
         if not self.shame:
             shame_meter = ""
+
         shame_meter = len(self.shame) + 1
         if shame_meter > SHAME_THRESHOLD:
             self.action = Action.ARCHIVE
+        elif self.is_start_date_tomorrow():
+            self.action = Action.START
         else:
             self.action = Action.SHAME
-            self.upcoming_shame = SHAME_CHAR * shame_meter
+
+        self.upcoming_shame = SHAME_CHAR * shame_meter
 
     def __repr__(self):
         return f"{__class__.__name__}({self.front_spaces} [{self.marker}] {self.text})"
@@ -137,14 +200,15 @@ def get_file_content(filename:str, directory=DN_DIR):
     filename = f"{directory}/{filename}"
     return open(filename, "r+").read().rstrip()
 
-def find_pattern_in_file(filename, pattern):
+def find_pattern_in_file(notename, pattern):
     matching_lines = []
-    note_text = get_file_content(filename)
+    note_text = get_file_content(notename)
     for line in note_text.split("\n"):
         m = re.search(pattern, line)
         if m and m.group(0):
             matching_lines.append(
                 Todo(raw_text=m.group(0),
+                notename=notename,
                 front_spaces=m.group(1),
                 todo_marker=m.group(2),
                 todo_shame=m.group(3),
@@ -164,17 +228,28 @@ def find_pattern_in_files(FILE_DIR, pattern):
 
 def add_shame_or_archive(todos: List[str], original_note_name=None):
     """Format todos for the new note"""
+    # breakpoint()
     formatted_todos= []
+    future_todos = []
     to_be_archived = []
     for todo in todos:
-        new_todo = f"{todo.front_spaces} - [ ] {todo.upcoming_shame} {todo.text}"
-        if todo.action == Action.SHAME:
+        new_todo = f"{todo.front_spaces}- [ ] {todo.upcoming_shame} {todo.text}"
+        if todo.action in [Action.SHAME, Action.STICK, Action.START]:
             formatted_todos.append(new_todo)
+        elif todo.action == Action.FUTURE:
+            # if the future todos are to be hidden from the DN
+            if HIDE_FUTURE_TODOS_FROM_DAILY_NOTE:
+                continue
+            else:
+                future_todos.append(new_todo)
         elif todo.action == Action.ARCHIVE:
             # add a backlink to original note
             new_todo += f" [[{original_note_name}]]"
             to_be_archived.append(new_todo)
-    return formatted_todos, to_be_archived
+
+    # Add the future todos at the end to minimise distraction
+    arranged_todos = formatted_todos + future_todos
+    return arranged_todos, to_be_archived
 
 def write_file(notename, content, directory=DN_DIR):
     """Write out file in daily note directory
@@ -223,8 +298,6 @@ def get_open_todos(notename):
     filename = f"{notename}.md"
     open_todos = find_pattern_in_file(filename, OPEN_TASK_PATTERN)
     # check if there are any backlinked todos:
-    backlinked_todos = get_backlink_todos(filename)
-    open_todos.extend(backlinked_todos)
     print(f"{len(open_todos)} open todos found in {filename}")
     return open_todos
 
@@ -255,19 +328,22 @@ def main():
     """Tommorrow note will not have the archived todos
     """
     today_note_name = get_note_name_for("today")
+    tmrw_note_name = get_note_name_for("tomorrow")
     today_todos = get_open_todos(today_note_name)
-    shamed_todos, to_be_archived_todos = add_shame_or_archive(today_todos, today_note_name)
+    tmrw_backlinked_todos = get_backlink_todos(tmrw_note_name)
+    shamed_todos, to_be_archived_todos = add_shame_or_archive(today_todos + tmrw_backlinked_todos, today_note_name)
     # Add todos to tomorrow's note and write it out to a file
-    # tmrw_note_name = get_note_name_for("tomorrow")
-    # templatified_note = add_content_to_note_template(tmrw_note_name, shamed_todos)
-    # write_file(tmrw_note_name, templatified_note)
-    # # Make sure that we close out on pending tasks
-    # modified_today_note = replace_open_with_moved_todos(today_note_name)
-    # write_file(today_note_name, modified_today_note)
+    templatified_note = add_content_to_note_template(tmrw_note_name, shamed_todos)
+    write_file(tmrw_note_name, templatified_note)
+    # Make sure that we close out on pending tasks
+    modified_today_note = replace_open_with_moved_todos(today_note_name)
+    write_file(today_note_name, modified_today_note)
 
     # Now add stuff to archive
     all_archived_todos = get_all_archived_todos(to_be_archived_todos)
     archive_content = write_to_archive_template(all_archived_todos)
     write_file(ARCHIVE_NOTE_NAME, archive_content)
+
+
 if __name__ == "__main__":
     main()
